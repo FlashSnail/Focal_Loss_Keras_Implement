23754c8615a0e71b50b176f95a3c443658b0db00
Zehua Zhang <zzh_0729@foxmail.com>
2018-09-20 17:34:21 +0800

    Update focalloss.py

--- focalloss.py
+++ focalloss.py
@@ -1,7 +1,10 @@
-def focal_loss(gamma=2, alpha=0.75):
-	def focal_loss_fixed(y_true, y_pred):#with tensorflow
+import os
+os.environ['KERAS_BACKEND'] = 'tensorflow'
+from keras import backend as K
+def focal_loss(gamma=2, alpha=0.25):
+	def focal_loss_fixed(y_true, y_pred):
 		eps = 1e-12
-		y_pred=K.clip(y_pred,eps,1.-eps)#improve the stability of the focal loss and see issues 1 for more information
+		y_pred=K.clip(y_pred,eps,1.-eps)
         	pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
         	pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
         	return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1))-K.sum((1-alpha) * K.pow( pt_0, gamma) * K.log(1. - pt_0))
